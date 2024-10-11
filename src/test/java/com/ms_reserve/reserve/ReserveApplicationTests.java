a96c06d0e6fe38232e0ef069d2f33a94a1bbd939
Carlos Palacios <carlospalaciosmena9@gmail.com>
2024-10-11 12:13:19 -0500

    Initial commit

---
+++ src/test/java/com/ms_reserve/reserve/ReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_reserve.reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
